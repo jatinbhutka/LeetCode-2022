a56a3e7a6b05661c651f3257a7c672fcaa660e7c
Jatin Bhutka <jatinbhutka@gmail.com>
2022-01-28 00:09:00 -0800

    Create 283. Move Zeroes.py

---
+++ Microsoft/283. Move Zeroes.py
@@ -0,0 +1,64 @@
+"""
+
+# 283. Move Zeroes
+Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+Note that you must do this in-place without making a copy of the array.
+
+ 
+
+Example 1:
+
+Input: nums = [0,1,0,3,12]
+Output: [1,3,12,0,0]
+Example 2:
+
+Input: nums = [0]
+Output: [0]
+ 
+
+Constraints:
+
+1 <= nums.length <= 104
+-231 <= nums[i] <= 231 - 1
+ 
+
+Follow up: Could you minimize the total number of operations done?
+
+"""
+# Solution 1: Using Pointer at Zero
+
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        i_non = 0
+        i = 0
+        while i < len(nums):
+            if nums[i] != 0:
+                nums[i_non] = nums[i]
+                i_non += 1
+                i += 1
+            else:
+                i +=1
+        for i in range(i_non, len(nums)):
+            nums[i] = 0
+        return nums
+      
+      
+# Solution 2: Using Pointer at Zero
+      
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        zero_th = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[i], nums[zero_th] = nums[zero_th], nums[i]
+                zero_th += 1
+        return nums
+      
