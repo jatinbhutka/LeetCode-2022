f2953072d6f083b7e245fec960f7e9fda699cd5c
Jatin Bhutka <jatinbhutka@gmail.com>
2022-02-04 04:30:02 -0800

    Create 136. Single Number.py

---
+++ Microsoft/136. Single Number.py
@@ -0,0 +1,45 @@
+# 136. Single Number:
+
+'''
+Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+Example 1:
+Input: nums = [2,2,1]
+Output: 1
+
+Example 2:
+Input: nums = [4,1,2,1,2]
+Output: 4
+
+Example 3:
+Input: nums = [1]
+Output: 1
+
+Constraints:
+1 <= nums.length <= 3 * 104
+-3 * 104 <= nums[i] <= 3 * 104
+Each element in the array appears twice except for one element which appears only once.
+'''
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        dic = {}
+        for num in nums:
+            dic[num] = dic.get(num, 0)+1
+        for key, val in dic.items():
+            if val == 1:
+                return key
+                
+# Time: O(N)
+# Space: O(N)
+
+
+
+# Using Math:                 
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        return 2*sum(set(nums)) - sum(nums)       
+        
+# Time: O(N)
+# Space: O(1)
