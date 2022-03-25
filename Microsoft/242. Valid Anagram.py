3385716b5175cb66e4c012f8c05ec366c318fcb8
Jatin Bhutka <jatinbhutka@gmail.com>
2022-03-25 00:29:25 -0700

    Update 242. Valid Anagram.py

--- Microsoft/242. Valid Anagram.py
+++ Microsoft/242. Valid Anagram.py
@@ -1,6 +1,11 @@
+# 242. Valid Anagram:
+
+How to sort String?
+
+s = 'dcka'
+sorted(s) ---> 'acdk'
+
 """
-
-
 Given two strings s and t, return true if t is an anagram of s, and false otherwise.
 
 An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
